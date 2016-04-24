d30010fb71a25489903f9a189710cfc4c4cfecc4
diegoachy <tiran_o@hotmail.com>
2016-04-24 16:29:23 -0500

    archivo de express con sass

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
     exphbs  = require('express-handlebars');
 
 var app = express();
+
+// The static middleware must come after the sass middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // insertar las vistas con handlebar
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -9,11 +18,11 @@ app.set('view engine', 'handlebars');
 
 // route
 app.get("/", function(req, res){
-  res.render("home", {saludo: "hola diego achury"});
+  res.render("index");
 })
 
-app.get("/:nombre", function(req, res){
-  res.render("home", {nombre: req.params.nombre});
+app.get("/login", function(req, res){
+  res.render("login");
 });
 
 app.listen(8000);
